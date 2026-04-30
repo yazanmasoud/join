54383aaf61ed3dbae75abaf9fe3246a4aa5b54ac
Yazan Masoud <yazanmas895@gmail.com>
2026-04-30 16:01:25 +0200

    feat: add sign-up section with toggle between login and signup (WIP)

--- js/main.js
+++ js/main.js
@@ -18,4 +18,24 @@ function animateLogo(delay = 0) {
   }, delay);
 }
 
-animateLogo(1500);
+animateLogo(500);
+
+function openSignUp() {
+  const signup = document.getElementById("signup");
+  const login = document.getElementById("login");
+
+  if (!signup || !login) return;
+
+  signup.classList.remove("hidden"); // anzeigen
+  login.classList.add("hidden");     // verstecken
+}
+
+function openLogin() {
+  const signup = document.getElementById("signup");
+  const login = document.getElementById("login");
+
+  if (!signup || !login) return;
+
+  signup.classList.add("hidden");    // verstecken
+  login.classList.remove("hidden");  // anzeigen
+}
